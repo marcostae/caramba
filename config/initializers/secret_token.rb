d1b76d78b67595f055733323be321e5e2c7e8b53
marcostae <marcostae@gmail.com>
2014-07-17 08:50:04 -0300

    inicialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Porra::Application.config.secret_key_base = '5e4471716e30f7413267532801f8fd17d8594ecc2d7b096e4fb303fd68e8aa6b78819385edd6e1018679ec5e00ef2f04bc761f406e89d0da0aaec85b5467378d'
