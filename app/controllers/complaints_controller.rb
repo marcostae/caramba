0cfa9449cdeffd6b76255c8b50d2cd9c480ac042
marcostae <marcostae@gmail.com>
2014-08-20 09:34:58 -0300

    webservice

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -79,7 +79,7 @@ class ComplaintsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def complaint_params
-      # params.require(:complaint).permit(:user_id, :typegroup_id, :type_id, :photo, :comment, :latitude, :longitude)
-      params[:complaint]
+      params.require(:complaint).permit(:user_id, :typegroup_id, :type_id, :photo, :comment, :latitude, :longitude)
+      # params[:complaint]
     end
 end
