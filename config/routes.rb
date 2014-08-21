4cadc1848aa5574b54cad6a0886f136eb81d46ca
marcostae <marcostae@gmail.com>
2014-08-21 09:00:38 -0300

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Porra::Application.routes.draw do
   resources :complaints
   resources :typegroups
   resources :types
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
