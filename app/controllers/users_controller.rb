1df16752e874ee2719382b2818a7e4f097a6e993
hodoufo <hodoufo@gmail.com>
2014-09-28 16:28:09 -0300

    Login funcion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -87,6 +87,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params[:user]
+      params.require(:user).permit(:name, :email, :password, :motto, :avatar)
     end
 end
